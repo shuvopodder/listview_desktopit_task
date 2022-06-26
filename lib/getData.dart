be052d99b5cfa76840ea5ee157df793e1f42e0e7
Shuvo Podder <shuvopodder9@gmail.com>
2022-06-27 02:33:11 +0600

    Create getData.dart

---
+++ lib/getData.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: file_names
+
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class getData with ChangeNotifier{
+  List<ListData> data = [];
+  var pageNumber = 0;
+
+  Future getListData() async{
+    pageNumber += 1;
+    var response =
+    await http.get(
+        Uri.parse('https://picsum.photos/v2/list?page=$pageNumber&limit=20'));
+
+    var jsonData = json.decode(response.body);
+    for(var u in jsonData){
+      ListData list = ListData(u["id"],u["author"],u["width"],u["height"],u["url"]);
+      data.add(list);
+    }
+
+    notifyListeners();
+    return data;
+  }
+}
+
+class ListData{
+  final String id, author, url;
+  final int width, height;
+  ListData(this.id,this.author,this.width,this.height,this.url);
+}
